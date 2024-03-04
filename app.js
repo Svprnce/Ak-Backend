103f560a770e8f938bd422b200c58417ae039610
Sv Prince <svprnce0@gmail.com>
2024-03-04 21:41:07 +0530

    fixed cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ connectdatabase()
 
 
 app.use(cors({
-    origin : ["http://localhost:5173/",'http://localhost:5173','http://localhost:3000'],
+    origin : ["http://localhost:5173/",'http://localhost:5173','http://localhost:3000','https://akstorebysv.netlify.app/'],
     credentials : true,
 }))
 
--- config/database.js
+++ config/database.js
@@ -1,12 +1,16 @@
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose')
 
-const connectdatabase = async ()=>{
+const connectdatabase = async () => {
+    // const uri = "mongodb+srv://devsvprince:Abba@1221@akstore@akstore-cluster.wekpn2o.mongodb.net/?retryWrites=true&w=majority";
     try{
-        await mongoose.connect(process.env.MONGODB_URL + 'akstore')
+        // await mongoose.connect(process.env.MONGODB_URL + 'akstore')
+        await mongoose.connect(process.env.MONGODB_URL)
+
         console.log('database connected')
     }
     catch(e){
-        console.log('error on database')
+        console.log('error on database',e)
     }
 }
 
