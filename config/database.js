fc9986f60f54708158b195fc6fd4e3b0e34d10d8
Sv Prince <svprnce0@gmail.com>
2024-03-04 22:02:10 +0530

    Updated cors

--- config/database.js
+++ config/database.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose')
 
 const connectdatabase = async () => {
-    // const uri = "mongodb+srv://devsvprince:Abba@1221@akstore@akstore-cluster.wekpn2o.mongodb.net/?retryWrites=true&w=majority";
     try{
         // await mongoose.connect(process.env.MONGODB_URL + 'akstore')
         await mongoose.connect(process.env.MONGODB_URL)
